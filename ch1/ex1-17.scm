920119c0b0ad267f62313f5a86b14dfa5df3f44e
John Novak <john@johnnovak.net>
2014-05-14 20:30:39 +1000

    .

---
+++ ch1/ex1-17.scm
@@ -0,0 +1,16 @@
+; Calculate the product of two numbers in a logarithmic number of steps using
+; the double and halve procedures
+
+(define (double x)
+  (* x 2))
+
+(define (halve x)
+  (/ x 2))
+
+(define (fast-mul a b)
+  (define (mul-iter s a b)
+    (cond ((= b 0) s)
+          ((even? b) (mul-iter s (double a) (halve b)))
+          (else (mul-iter (+ s a) a (- b 1)))))
+  (mul-iter 0 a b))
+
