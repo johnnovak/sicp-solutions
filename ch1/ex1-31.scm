5cb29d55f1b10d5eb06138211100d147bbda99ad
John Novak <john@johnnovak.net>
2014-05-16 22:40:21 +1000

    .

--- ch1/ex1-31.scm
+++ ch1/ex1-31.scm
@@ -1,8 +1,7 @@
 ; Write a procedure called 'product' that returns the product of the values of
 ; a function at points over a given range
 
-(define (inc x)
-  (+ x 1))
+(define (inc x) (+ x 1))
 
 (define (product term a next b)
   (if (> a b)
@@ -18,8 +17,26 @@
   (iter a 1))
 
 
+; Compute the approximation of pi/4:
+; 1 -> 2*4 / 3^2
+; 2 -> 4*6 / 5^2
+; 3 -> 6*8 / 7^2
+; ...
+; n -> 2n * 2(n+1) / (n+1)^2
+;        4n(n+1)
+
+(define (approx-pi times)
+  (define (f n)
+    (/ (* 4 n (+ n 1))
+       (square (+ (* 2 n) 1))))
+  (* 4.0 (product-iter f 1 inc times)))
+
+
 ; Tests
 
 (product square 5 inc 11)           ; 2766234240000
 (product-iter square 5 inc 11)      ; 2766234240000
+
+(approx-pi 100)                     ; 3.1493784731686008
+(approx-pi 1000)                    ; 3.142377365093878
 
---
+++ ch1/ex1-32.scm
@@ -0,0 +1,40 @@
+; Write a procedure that combines a collection of terms, using some general
+; accumulation function. Show how sum and product can both be defined as
+; simple calls to accumulate.
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter acc a)
+    (if (> a b)
+        acc
+        (iter (combiner acc (term a))
+              (next a))))
+  (iter null-value a))
+
+(define (sum-iter term a next b)
+  (accumulate-iter + 0 term a next b))
+
+(define (product-iter term a next b)
+  (accumulate-iter * 1 term a next b))
+
+
+; Tests
+
+(sum square 5 1+ 11)                ; 476
+(product square 5 1+ 11)            ; 2766234240000
+
+(sum-iter square 5 1+ 11)           ; 476
+(product-iter square 5 1+ 11)       ; 2766234240000
+
